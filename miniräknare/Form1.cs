8d658d9762629caa1a27510d6c41ce73a2de6c88
gabrielm1709 <91737889+gabrielm1709@users.noreply.github.com>
2023-05-21 17:07:21 +0200

    Add files via upload

--- miniräknare/Form1.cs
+++ miniräknare/Form1.cs
@@ -129,43 +129,36 @@ namespace miniräknare
         private void button3_Click(object sender, EventArgs e)
         {
             textBox1.Text += "3";
-           
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
             textBox1.Text += "4";
-            
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
             textBox1.Text += "5";
-            
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
             textBox1.Text += "6";
-            
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
             textBox1.Text += "7";
-            
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
             textBox1.Text += "8";
-            
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
             textBox1.Text += "9";
-            
         }
 
         private void button15_Click(object sender, EventArgs e)
